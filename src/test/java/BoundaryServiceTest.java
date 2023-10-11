2c349d0766fbef11571c1e0c319e1b38ca329e63
PatesM <maximpates@gmail.com>
2023-10-11 23:36:57 +0500

    commit with failing BoundaryServiceTest test

--- src/test/java/BoundaryServiceTest.java
+++ src/test/java/BoundaryServiceTest.java
@@ -15,7 +15,7 @@ public class BoundaryServiceTest {
         return Stream.of(
                 Arguments.of(
                         new int[]{5, 4, 3, 2, 1},
-                        1
+                        5
                 )
         );
     }
