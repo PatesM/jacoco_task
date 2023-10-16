44f639a72463aad45440bffa653bea1a127164ea
PatesM <maximpates@gmail.com>
2023-10-16 22:47:26 +0500

    added test with low test coverage BoundaryServiceTest

--- src/test/java/BoundaryServiceTest.java
+++ src/test/java/BoundaryServiceTest.java
@@ -1,8 +1,11 @@
 import io.qameta.allure.Description;
 import io.qameta.allure.Story;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.maxqa.BoundaryService;
+
+import java.util.Arrays;
 
 @Story(value = "Testing the implementation of the BoundaryService class")
 public class BoundaryServiceTest {
@@ -16,6 +19,6 @@ public class BoundaryServiceTest {
 //        int[] values = {5, 4, 3, 2, 1};
 //        int expectedResult = Arrays.stream(values).min().getAsInt();
 //        int actualResult = boundaryService.findMin(values);
-//        Assertions.assertEquals(expectedResult, actualResult);
+//        Assertions.assertEquals(5, actualResult);
     }
 }
